d6974b6d690bd1ad8e29504dc5abef154d85c7f0
Julia Nissen <nisse053@umn.edu>
2018-03-11 19:07:20 -0500

    updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "recipe_db"
-});
+if (process.env.JawsDB_URL) {
+  connection = mysql.createConnection(process.env.JawsDB_URL);
+}
+
+else {
+  connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "recipe_db"
+  })
+}
 
 // Make connection.
 connection.connect(function(err) {
